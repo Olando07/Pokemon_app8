2a54085da6d63b8e48abe6a8aeac329ec0584a90
Olando McDonald <olandomcdonald07@gmail.com>
2025-06-24 00:07:01 -0500

    "fixed typo in file path"

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             <Routes basename="/Pokemon_app8">
                 <Route path="Pokemon_app8/" element={<Home></Home>}></Route>
                 <Route path="Pokemon_app8/guessing-game" element={<Game collection={collection} setCollection={setCollection}></Game>}></Route>
-                <Route path="Pokemon_app8/favourties" element={<Favourites collection={collection}></Favourites>}></Route>
+                <Route path="Pokemon_app8/favourites" element={<Favourites collection={collection}></Favourites>}></Route>
             </Routes>
         </>
     );
