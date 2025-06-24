4a791371a9d0580651336dc93b1d7d2b0d46d740
Olando McDonald <olandomcdonald07@gmail.com>
2025-06-24 07:26:10 -0500

    "more changes to paths"

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,13 +13,13 @@ function Navbar() {
                     <img src={pikachu} alt="pikachu" />
                 </div>
                 <div className="nav-buttons">
-                    <Link to="Pokemon_app8/">
+                    <Link to="/Pokemon_app8">
                         <button className="home-btn">
                             <img src={home} alt="" id="home" />
                             <p>Home</p>
                         </button>
                     </Link>
-                    <Link to="Pokemon_app8/favourites">
+                    <Link to="/Pokemon_app8/favourites">
                         <button className="favourites-btn">
                             <img src={star} alt="" id="favourite" />
                             <p>Your Collection</p>
