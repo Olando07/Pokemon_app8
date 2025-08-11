1b51b35d032edacdd18321faa2decc6bb47e6856
Olando McDonald <olandomcdonald07@gmail.com>
2025-08-10 21:57:53 -0500

    Fixes and updates to navbar made
    Added navigation button for game page, made  navbar sticky when user scrolls along with appropriate css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import pikachu from "../assets/pikachu3.png";
 import home from "../assets/lucario.png";
 import star from "../assets/pikachu_icon2.png";
+import game from "../assets/greninja_clear.png";
 
 function Navbar() {
     return (
@@ -17,6 +18,12 @@ function Navbar() {
                         <button className="home-btn">
                             <img src={home} alt="" id="home" />
                             <p>Home</p>
+                        </button>
+                    </Link>
+                    <Link to="/Pokemon_app8/guessing-game">
+                        <button className="game-btn">
+                            <img src={game} alt="" id="game" />
+                            <p>Play</p>
                         </button>
                     </Link>
                     <Link to="/Pokemon_app8/favourites">
